f8a5184dc4092e158fb989fc31bcee3ed342c5c1
Simon Tabor <me@simontabor.com>
2012-07-20 15:53:42 +0100

    formatted

--- gosquared.js
+++ gosquared.js
@@ -1,20 +1,16 @@
-(function($){
+(function($) {
   $.fn.track = function(p) {
-  	if (typeof p != 'object') return 'Please provide an object - required events and name'
-  	p.params = (typeof p.params == 'object') ? p.params : {};
-  	if ((typeof p.events != 'string') || (typeof p.name != 'string')) return 'Please provide at least one event and a name as a string';
-  	this.on(p.events,function(e) {
-  		if ($.isFunction(p.beforeSend)) p.beforeSend(e);
-  		console.log(p.name + ' sent off with params '+p.params);
+    if (typeof p != 'object') return 'Please provide an object - required events and name'
+    p.params = (typeof p.params == 'object') ? p.params : {};
+    if ((typeof p.events != 'string') || (typeof p.name != 'string')) return 'Please provide at least one event and a name as a string';
+    this.on(p.events, function(e) {
+      if ($.isFunction(p.beforeSend)) p.beforeSend(e);
+      console.log(p.name + ' sent off with params ' + p.params);
       if (!GoSquared) GoSquared = {};
       if (!GoSquared.q) GoSquared.q = [];
       GoSquared.q.push('TrackEvent', p.name, p.params);
-  		if ($.isFunction(p.afterSend)) p.afterSend(e);
-  		return true;
-  	});
+      if ($.isFunction(p.afterSend)) p.afterSend(e);
+      return true;
+    });
   };
 })(jQuery);
-
-
-
-
