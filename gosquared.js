39860974ff8219efe563cf9e3dbf905a42dc0ea6
simontabor <me@simontabor.com>
2012-12-07 10:54:09 +0000

    added api wrapper, need to sort out non-default functions

--- gosquared.js
+++ gosquared.js
@@ -13,4 +13,52 @@
       return true;
     });
   };
+
+  var opts = {};
+
+  // makes sure the opts and the callbacks are right
+  var parse = function(opts,cb) {
+    if ($.isFunction(opts)) {
+      // opts is the callback
+      return [{},opts];
+    }
+    return [opts || {},cb || function(){}];
+  };
+
+  var defaultFunction = function(func,params,cb) {
+    var o = parse(params,cb); params = o[0]; cb = o[1];
+    get(func,params,cb);
+  };
+
+  var defaultFunctions = ["aggregateStats","alertPreferences","campaigns","concurrents","engagement","events","expandUrl","fullDump","functions","geo","ignoredVisitors","notifications","organics","overview","pages","referrers","reportPreferences","sites","time","timeSeries","trends","visitors"];
+
+  var get = function(endpoint, params, cb) {
+
+    $.ajax({
+      url: 'https://api.gosquared.com/v2/'+endpoint,
+      timeout: params.timeout || opts.timeout || 2000,
+      data: $.extend({},opts,params),
+      success: function(data) {
+        cb(null,data);
+      },
+      error: function(xhr) {
+        var responseText = $.parseJSON(xhr.responseText);
+        cb(responseText);
+      },
+      type: 'GET',
+      dataType: 'json'
+    });
+
+  };
+
+  $.GoSquared = function(config) {
+    opts = config;
+    return this;
+  };
+
+  for (var i = 0; i< defaultFunctions.length; i++) {
+    $.GoSquared[defaultFunctions[i]] = defaultFunction.bind(this,defaultFunctions[i]);
+  }
+
+  
 })(jQuery);
